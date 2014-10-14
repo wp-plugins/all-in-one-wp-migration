88c3caf85f44d9047054de20f0bd501e39c77646
bangelov <bangelov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-14 22:47:49 +0000

    Tagging version 2.0.3
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-wp-migration/tags/2.0.3@1007591 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- all-in-one-wp-migration.php
+++ all-in-one-wp-migration.php
@@ -5,7 +5,7 @@
  * Description: Migration tool for all your blog data. Import or Export your blog content with a single click.
  * Author: ServMask
  * Author URI: https://servmask.com/
- * Version: 2.0.2
+ * Version: 2.0.3
  *
  * Copyright (C) 2014 ServMask Inc.
  *
