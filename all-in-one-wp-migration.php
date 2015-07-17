7ad73e1630c91a88d5d41c635e1f17e7ca4202db
bangelov <bangelov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-17 19:30:50 +0000

    Tagging version 3.7
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-wp-migration/tags/3.7@1201194 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- all-in-one-wp-migration.php
+++ all-in-one-wp-migration.php
@@ -5,7 +5,7 @@
  * Description: Migration tool for all your blog data. Import or Export your blog content with a single click.
  * Author: ServMask
  * Author URI: https://servmask.com/
- * Version: 3.3
+ * Version: 3.7
  * Text Domain: all-in-one-wp-migration
  * Domain Path: /languages
  * Network: True
@@ -48,7 +48,10 @@ define( 'AI1WM_URL', plugins_url( '', __FILE__ ) );
 define( 'AI1WM_STORAGE_URL', plugins_url( 'storage', __FILE__ ) );
 
 // Plugin Backups Url
-define( 'AI1WM_BACKUPS_URL', plugins_url( 'backups', __FILE__ ) );
+define( 'AI1WM_BACKUPS_URL', content_url( 'ai1wm-backups', __FILE__ ) );
+
+// Themes Absolute Path
+define( 'AI1WM_THEMES_PATH', get_theme_root() );
 
 // include constants
 require_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'constants.php';
